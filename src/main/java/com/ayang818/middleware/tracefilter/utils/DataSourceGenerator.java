e9df80fa07b6448d5b5b439cd77b0eb1268fcfcb
杨丰畅 <ayang818@qq.com>
2020-05-06 15:49:10 +0800

    (dev)使用内存映射技术提升了1G到2G大小文件的写入速度

--- src/main/java/com/ayang818/middleware/tracefilter/utils/DataSourceGenerator.java
+++ src/main/java/com/ayang818/middleware/tracefilter/utils/DataSourceGenerator.java
@@ -2,17 +2,11 @@ package com.ayang818.middleware.tracefilter.utils;
 
 import java.io.*;
 import java.nio.ByteBuffer;
+import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
-import java.util.HashSet;
-import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.Executor;
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadPoolExecutor;
 
 /**
  * @author 杨丰畅
@@ -21,9 +15,9 @@ import java.util.concurrent.ThreadPoolExecutor;
  **/
 public class DataSourceGenerator {
 
-    private static final String PATH = "D:/middlewaredata/data.txt";
+    private static final String BASE_DIR = "D:/middlewaredata/";
 
-    private static final long TARGET_FILE_SIZE = 1024 * 1024 * 100;
+    private static final long TARGET_FILE_SIZE = 1024 * 1024 * 1000;
 
     private static final Executor SINGLE_FLUSH_THREAD = Executors.newSingleThreadExecutor();
 
@@ -31,18 +25,90 @@ public class DataSourceGenerator {
 
     public static void main(String[] args) {
         DataSourceGenerator generator = new DataSourceGenerator();
-        generator.generate();
-        generator.bioGenerate();
+        generator.mmapGenerate();
+        //generator.nioGenerate();
+        //generator.bioGenerate();
     }
 
-    public void generate() {
+    /**
+     * 使用内存映射技术进行文件写入
+     */
+    public void mmapGenerate() {
+        String fileName = "mmap_data.txt";
         long start = System.currentTimeMillis();
         System.out.println("开始生成数据");
 
-        Set<String> idSet = new HashSet<>();
-        ByteBuffer byteBuffer = ByteBuffer.allocateDirect(1024 * 4);
+        try (RandomAccessFile memoryAccessFile = new RandomAccessFile(BASE_DIR + fileName, "rw")) {
+            FileChannel fileChannel = memoryAccessFile.getChannel();
+            MappedByteBuffer mappedByteBuffer = fileChannel.map(FileChannel.MapMode.READ_WRITE, 0, TARGET_FILE_SIZE);
+            int position = 0;
+            while (true) {
+                byte[] bytes = generateLine().getBytes();
+                if (position + bytes.length > TARGET_FILE_SIZE) break;
+                mappedByteBuffer.position(position);
+                mappedByteBuffer.put(bytes);
+                position += bytes.length;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-        File dataFile = new File(PATH);
+        long delta = System.currentTimeMillis() - start;
+        System.out.println("[内存映射] : 数据生成完成");
+        System.out.println("花费 " + delta + " 毫秒，速度为 " + String.format("%.2f", ((double) TARGET_FILE_SIZE / (1024 * 1024)) / (delta / 1000)) + " MB/S");
+    }
+
+    /**
+     * 带缓冲区的bio，目前看来这是表现最好的方式了
+     */
+    public void bioGenerate() {
+        String fileName = "bio_data.txt";
+        long start = System.currentTimeMillis();
+        System.out.println("开始生成数据");
+
+        File dataFile = new File(BASE_DIR + fileName);
+        BufferedWriter bufferedWriter = null;
+        try {
+            bufferedWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(dataFile)));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        while (!dataFile.exists() || dataFile.length() < TARGET_FILE_SIZE) {
+            // 若文件不存在，则创建
+            if (!dataFile.exists()) {
+                try {
+                    dataFile.createNewFile();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            // 生成一行数据
+            String lineString = generateLine();
+            try {
+                bufferedWriter.write(lineString);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        long delta = System.currentTimeMillis() - start;
+        System.out.println("[缓冲区bio] : 数据生成完成");
+        System.out.println("花费 " + delta + " 毫秒，速度为 " + String.format("%.2f", ((double) TARGET_FILE_SIZE / (1024 * 1024)) / (delta / 1000)) + " MB/S");
+    }
+
+
+    /**
+     * nio + 缓冲区
+     */
+    public void nioGenerate() {
+        String fileName = "nio_data.txt";
+        long start = System.currentTimeMillis();
+        System.out.println("开始生成数据");
+
+        ByteBuffer byteBuffer = ByteBuffer.allocateDirect(1024 * 32);
+
+        File dataFile = new File(BASE_DIR + fileName);
         FileOutputStream fileOutputStream = null;
         FileChannel outputChannel = null;
 
@@ -64,7 +130,7 @@ public class DataSourceGenerator {
             }
 
             // 生成一行数据
-            String lineString = generateLine(idSet);
+            String lineString = generateLine();
             byte[] lineBytes = lineString.getBytes();
             // 判断缓冲区是否足够
             if (byteBuffer.remaining() < lineBytes.length) {
@@ -100,13 +166,12 @@ public class DataSourceGenerator {
     }
 
     /**
-     * @param idSet
      * @return
      * @description traceId(全局唯一Id)|startTime(调用开始的时间)|spanId(调用链环节某条数据Id)|parentSpanId(某条数据的父节点Id)
      * |duration(调用耗时)|serviceName(调用的服务名)|spanName(调用的埋点名)|host(机器标识，比如ip，机器名)|tags|
      * 共 9 种
      */
-    public String generateLine(Set<String> idSet) {
+    public String generateLine() {
         StringBuilder res = new StringBuilder();
         UUID uuid = UUID.randomUUID();
 
@@ -129,79 +194,6 @@ public class DataSourceGenerator {
         res.append("\n");
 
         return res.toString();
-    }
-
-    /**
-     * 带缓冲区的bio
-     */
-    public void bioGenerate() {
-        long start = System.currentTimeMillis();
-        System.out.println("开始生成数据");
-
-        String suffix = "_bio";
-        File dataFile = new File(PATH + suffix);
-        Set<String> idSet = new HashSet<>();
-        BufferedWriter bufferedWriter = null;
-        try {
-            bufferedWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(dataFile)));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-
-        while (!dataFile.exists() || dataFile.length() < TARGET_FILE_SIZE) {
-            // 若文件不存在，则创建
-            if (!dataFile.exists()) {
-                try {
-                    dataFile.createNewFile();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            // 生成一行数据
-            String lineString = generateLine(idSet);
-            try {
-                bufferedWriter.write(lineString);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-        long delta = System.currentTimeMillis() - start;
-        System.out.println("[缓冲区bio] : 数据生成完成");
-        System.out.println("花费 " + delta + " 毫秒，速度为 " + String.format("%.2f", ((double) TARGET_FILE_SIZE / (1024 * 1024)) / (delta / 1000)) + " MB/S");
-    }
-
-    /**
-     * 没有 Buffer 的 nio 写入速度对比，这个速度是最慢的
-     */
-    public void noBufferGenerate() {
-        long start = System.currentTimeMillis();
-        System.out.println("开始生成数据");
-
-        String suffix = "_nobuffer";
-        File dataFile = new File(PATH + suffix);
-        Set<String> idSet = new HashSet<>();
-
-        while (!dataFile.exists() || dataFile.length() < TARGET_FILE_SIZE) {
-            // 若文件不存在，则创建
-            if (!dataFile.exists()) {
-                try {
-                    dataFile.createNewFile();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-
-            // 生成一行数据
-            String lineString = generateLine(idSet);
-            try {
-                Files.write(Paths.get(PATH + suffix), lineString.getBytes(), StandardOpenOption.APPEND);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        System.out.println("[无缓冲区nio] : 数据生成完成");
-        System.out.println("花费 " + (System.currentTimeMillis() - start) + " 毫秒");
     }
 
 }
