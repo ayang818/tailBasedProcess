ab549a511ea8c574d2ae5a79295235912bbc114a
ayang818 <ayang818@qq.com>
2020-05-14 11:06:09 +0800

    (dev)构思错误行处理逻辑，使用出现既顺延的方式来保证本机数据尽量完整

--- src/main/java/com/ayang818/middleware/tracefilter/io/impl/DataStreamHandlerImpl.java
+++ src/main/java/com/ayang818/middleware/tracefilter/io/impl/DataStreamHandlerImpl.java
@@ -2,7 +2,6 @@ package com.ayang818.middleware.tracefilter.io.impl;
 
 import com.ayang818.middleware.tracefilter.io.DataStreamHandler;
 import com.ayang818.middleware.tracefilter.utils.SplitterUtil;
-import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -12,6 +11,10 @@ import java.io.InputStream;
 import java.nio.ByteBuffer;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @author 杨丰畅
@@ -46,6 +49,11 @@ public class DataStreamHandlerImpl implements DataStreamHandler {
 
     private static final String BAN_ERROR_CODE = "1";
 
+    private static final ConcurrentHashMap<String, Node> TRACEID_MAP =
+            new ConcurrentHashMap<>();
+
+    private static final HashMap<String, Integer> tmpMap = new HashMap<>();
+
     @Override
     public void handleDataStream(InputStream dataStream) {
         logger.info("连接数据源成功，开始拉取数据......");
@@ -65,7 +73,7 @@ public class DataStreamHandlerImpl implements DataStreamHandler {
         long startTime = System.currentTimeMillis();
         byte[] bytes = new byte[defaultReadBufferSize];
         StringBuilder strbuilder = new StringBuilder();
-        // 总字节数，用于统计数据是否有坑
+        // 总字节数，用于统计数据是否有坑 + 为后续多线程拉取速度对比提供分段标准
         long sumbytes = 0;
         // 行号
         int lineNumber = 0;
@@ -84,22 +92,18 @@ public class DataStreamHandlerImpl implements DataStreamHandler {
 
             for (int i = 0; i < chars.length; i++) {
                 if (chars[i] == NEXT_LINE_FLAG) {
-                    // 当前处于第 count 条数据，一条traceId中的窗口大约为 2w 条数据
+                    // 行号
                     lineNumber += 1;
                     // 得到一行数据
                     String lineData = strbuilder.toString();
                     // 统计收到的字节数，这个没啥用
                     sumbytes += lineData.getBytes().length;
-
-                    // TODO : how to handle each line data
+                    // TODO 处理数据 返回是否为错误行
                     wrongLineNumber += handleLine(lineData, lineNumber) ? 1 : 0;
                     strbuilder.delete(0, strbuilder.length());
                 } else {
                     strbuilder.append(chars[i]);
                 }
-            }
-            if (lineNumber == 1514951) {
-                System.out.println(tmpstr);
             }
         }
         logger.info("共有 {} 行数据出错", wrongLineNumber);
@@ -109,28 +113,33 @@ public class DataStreamHandlerImpl implements DataStreamHandler {
 
     @Override
     public boolean handleLine(String line, Integer lineNumber) {
-        // 每行数据总共9列，以8个 | 号做分割
         if (line == null) {
             logger.info("此行为空, 行号 {}", lineNumber);
             return false;
         }
+        // 每行数据总共9列，以8个 | 号做分割
         String[] data = SplitterUtil.baseSplit(line, "\\|");
-        // 去除可能的
+        // 去除掉可能导致程序报错的数据，这一部分数据过滤掉应该不会对我的代码造成太大的影响
         if (data.length < 9) {
             logger.info("不符合格式的数据 : {}, 行号 {}", line, lineNumber);
             return false;
         }
+
         String traceId = data[0];
         String tags = data[8];
-        String[] eachTag = SplitterUtil.baseSplit(tags, "&");
+
         boolean isWrong = false;
-        // 遍历所有 tag，找到是否有符合要求的 tag，如果有则将这条有问题的 span 所在的 traceId 上报给通信中心
+
+        // 遍历所有 tag，查看这条span是否符合过滤要求
+        String[] eachTag = SplitterUtil.baseSplit(tags, "&");
+
         for (int i = 0; i < eachTag.length; i++) {
             String[] kvArray = SplitterUtil.baseSplit(eachTag[i], "=");
             if (kvArray != null && kvArray.length == 2) {
                 String key = kvArray[0];
                 String val = kvArray[1];
-                boolean satisfyStatusCode = STATUS_CODE.equals(key) && !PASS_STATUS_CODE.equals(val);
+                boolean satisfyStatusCode =
+                        STATUS_CODE.equals(key) && !PASS_STATUS_CODE.equals(val);
                 boolean satisfyError = ERROR.equals(key) && BAN_ERROR_CODE.equals(val);
                 // 过滤条件 : http.status_code!=200 || error=1
                 if (satisfyStatusCode || satisfyError) {
@@ -139,18 +148,25 @@ public class DataStreamHandlerImpl implements DataStreamHandler {
                 }
             }
         }
+
         if (isWrong) {
             //System.out.printf("发现错误数据 %s, 行号 %d\n", line, lineNumber);
-            // TODO : 发现错误数据之后的处理 ：1. 上报数据中心。 2. 窗口期向后延长 500(暂定)。 3. 本地落盘数据删除。
-            reportTraceId(traceId);
+            // TODO : 发现错误数据之后的处理 ：1. 上报数据中心。 2. 窗口期向后延长 1000(暂定)。
+
+            // 这里由于已经知道了行号和数据了，这里可以尝试使用线程池来加速，防止IO阻塞时间过长导致效率下降
+            handleWrongLine(traceId, line, lineNumber);
             return true;
         }
         return false;
     }
 
-    private void reportTraceId(String traceId) {
-
+    private void handleWrongLine(String traceId, String lineData, Integer lineNumber) {
+        // TRACEID_MAP.computeIfPresent(traceId, );
     }
 
 
+}
+
+class Node {
+    public AtomicInteger showNum;
 }
