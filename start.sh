2d5da009af0f73b4bbeedad742f29f49a7dd01b7
ayang818 <ayang818@qq.com>
2020-06-10 16:42:52 +0800

    扩大堆内存

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 if   [  $SERVER_PORT  ];
 then
-   java -Dserver.port=$SERVER_PORT -Xmx640m -Xms640m -jar /usr/local/src/app.jar &
+   java -Dserver.port=$SERVER_PORT -Xmx1024m -Xms1024m -jar /usr/local/src/app.jar &
 else
    java -Dserver.port=8000 -jar /usr/local/src/app.jar &
 fi
