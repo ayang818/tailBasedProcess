2778af517be6e1a28853efc963681bbbafb4e176
ayang818 <ayang818@qq.com>
2020-06-24 18:11:05 +0800

    10个一组处理最快貌似

--- src/main/java/com/ayang818/middleware/tailbase/client/ClientDataStreamHandler.java
+++ src/main/java/com/ayang818/middleware/tailbase/client/ClientDataStreamHandler.java
@@ -136,7 +136,7 @@ public class ClientDataStreamHandler implements Runnable {
                 , json, pos);
         updateDataQueue.offer(msg);
 
-        if (updateDataQueue.size() >= 15 || isFin) {
+        if (updateDataQueue.size() >= 10 || isFin) {
             StringBuilder res = new StringBuilder();
             res.append("{");
             res.append(String.format("\"type\": %d,", Constants.UPDATE_TYPE)).append("\"data\": [");
